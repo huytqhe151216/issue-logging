bb9c24ddb14ba3c352a349984c2713e3f54384ec
rb049 <huyheotmhp2001@gmail.com>
2024-12-13 16:18:56 +0700

    init

---
+++ src/main/java/com/rabiloo/issue_logging/IssueLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.rabiloo.issue_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueLoggingApplication.class, args);
+	}
+
+}
